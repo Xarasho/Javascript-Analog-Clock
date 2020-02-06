b3cdc8ad100abf31b7e2419ce1143b19ff3d2a9b
Xarasho <alex.olmedo.rojas@gmail.com>
2020-02-05 19:13:49 -0600

    correcting variables names

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ function setClock() {
     const currentDate = new Date()
     const secondsRate = currentDate.getSeconds() / 60
     const minutesRate = (secondsRate + currentDate.getMinutes()) / 60
-    const HourssRate = (minutesRate + CurrentDate.getHours()) / 12
+    const hoursRate = (minutesRate + CurrentDate.getHours()) / 12
 }
