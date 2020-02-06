5eb3fcb85af530921821cc3343045095b7f78a30
Xarasho <alex.olmedo.rojas@gmail.com>
2020-02-05 19:12:12 -0600

    Adjusting correct time settings to clock arrows

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ setInterval(setClock, 1000)
 
 function setClock() {
     const currentDate = new Date()
-    const secondsRatio = currentDate.getSeconds() / 60
-    const minutesRatio = currentDate.getSeconds() / 60
-    const HoursRatio = currentDate.getSeconds() / 60 
+    const secondsRate = currentDate.getSeconds() / 60
+    const minutesRate = (secondsRate + currentDate.getMinutes()) / 60
+    const HourssRate = (minutesRate + CurrentDate.getHours()) / 12
 }
