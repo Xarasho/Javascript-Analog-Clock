75edaed7d8433b3c876a9efaf771ed1c3c90e6c2
Xarasho <alex.olmedo.rojas@gmail.com>
2020-02-05 19:14:30 -0600

    correcting variables names

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ setInterval(setClock, 1000)
 
 function setClock() {
     const currentDate = new Date()
-    const secondsRate = currentDate.getSeconds() / 60
-    const minutesRate = (secondsRate + currentDate.getMinutes()) / 60
-    const hoursRate = (minutesRate + CurrentDate.getHours()) / 12
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRate + currentDate.getMinutes()) / 60
+    const hoursRatio = (minutesRate + CurrentDate.getHours()) / 12
 }
